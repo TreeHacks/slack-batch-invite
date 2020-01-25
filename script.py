af4e2de959ab9017c759a270f334cba4d748e151
Arjun Karanam <akaranam@stanford.edu>
2020-01-24 17:38:29 -0800

    Added Name Functionality

--- script.py
+++ script.py
@@ -26,10 +26,10 @@ import csv
 
 
 #Constants
-token = "XXX"
-channel = "C0000000"
-fileName = "email.csv"
-writeName = "emailDone.csv"
+token = "xoxp-908939173762-923938115046-921726053936-53f9396973a04bf9e2c3d0577b35a9cc"
+channel = "CT3MZKFB7"
+fileName = "emails.csv"
+writeName = "emailsDone.csv"
 
 def inputEmails():
 	with open(fileName) as file_in:
@@ -38,8 +38,8 @@ def inputEmails():
 
 ONE_MINUTE = 60
 @limits(calls = 240, period = ONE_MINUTE)
-def APICall(email):
-	response = requests.get('https://slack.com/api/users.admin.invite?token=' + token + '&email=' + email + "&channels=" + channel)
+def APICall(email, real_name):
+	response = requests.get('https://slack.com/api/users.admin.invite?token=' + token + '&email=' + email + "&channels=" + channel + "&real_name=" + real_name)
 
 	if response.status_code == 200:
 		return json.loads(response.content.decode('utf-8'))
@@ -52,17 +52,20 @@ def batchInvite():
 		reader = csv.reader(csvFile, delimiter=',', quotechar='"')
 		for row in reader:
 			email = str(row[0])
-			if row[1] != 1:
+			real_name = str(row[1])
+			if row[2] != 1:
 				print(email)
+				print(real_name)
 				work = 0
 
 				while (work == 0):
 					try:
-						result = APICall(email)
+						result = APICall(email, real_name)
+						print(result)
 						if result["ok"] == False:
 							if result["error"] == "invite_limit_reached":
 								time.sleep(0.2)
-							elif result["error"] == "already_in_team" or result["error"] == "already_invited":
+							elif result["error"] == "already_in_team" or result["error"] == "already_invited" or result["error"] == "already_in_team_invited_user":
 								work = 1
 
 						elif result["ok"] == True:
