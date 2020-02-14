8d4b7e928940b4c3d6acc63161795cb5921a6901
Arjun Karanam <akaranam@stanford.edu>
2020-02-13 17:43:26 -0800

    Actually commented

--- script.py
+++ script.py
@@ -41,16 +41,19 @@ def tryEmail(email, real_name):
 			if result["ok"] == False:
 				if result["error"] == "invite_limit_reached":
 					time.sleep(0.2)
+				#if already in team, still mark it as done, just don't send out an API call
 				elif result["error"] == "already_in_team" or result["error"] == "already_invited" or result["error"] == "already_in_team_invited_user" or result["error"] == "internal_error":
 					with open(writeName, 'a') as f:
 					    writer = csv.writer(f)
 					    writer.writerow([email, real_name, result["error"]])
 					return
 				else:
+					#if another error, break and alert me
 					errorMessage = email + " failed because of: " + str(result["error"])
 					sys.exit(errorMessage)
 
 
+			#If it's good, send out the api call, and mark it as done by writing to CSV
 			elif result["ok"] == True:
 				with open(writeName, 'a') as f:
 				    writer = csv.writer(f)
@@ -64,19 +67,20 @@ def tryEmail(email, real_name):
 def batchInvite():
 	with open(fileName) as csvFile:
 		reader = csv.reader(csvFile, delimiter=',', quotechar='"')
-		next(reader)
+		next(reader) #skips the first row (header)
 		for row in reader:
 			email = str(row[2])
 			real_name = str(row[3])
 			if email not in readEmails:
-				print(email)
-				print(real_name)
+				print(email) #For debugging
+				print(real_name) #For debugging
 				tryEmail(email, real_name)
 
 
 		
 			    	
 def main():
+	#This first read is done so we have a list of emails that have already been read
 	with open(writeName) as csvFile:
 		ogReader = csv.reader(csvFile, delimiter=',', quotechar='"')
 		for row in ogReader:
